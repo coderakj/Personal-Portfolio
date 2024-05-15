944135c6cf8ec5e25025fe0d9a765832bc5e855e
Akarsh Jain <107072822+coderakj@users.noreply.github.com>
2024-05-15 15:33:44 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const githubBtn = document.getElementById("githubBtn");
+
+// Add event listener to the button
+githubBtn.addEventListener("click", function() {
+    // Open GitHub account link in a new tab
+    window.open("https://github.com/coderakj");
+});
